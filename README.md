65402e2cfb032c7be0d285b4ac71aa0ab621c5de
Andreas Weber <weber.andreas@gmail.com>
2021-08-31 21:29:32 +0200

    feat: init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BluePaint
+
+
+Theme Based in Github Plus
+
+<p align="center">
+<img src="https://raw.githubusercontent.com/AnWeber/bluepaint/main/preview.png" alt="Bluepaint" />
+</p>
+
---
+++ demo/vuets.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button v-on:click="decrement">-</button>
+    {{ count }}
+    <button v-on:click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+
+// Define the component in class-style
+@Component
+export default class Counter extends Vue {
+  // Class properties will be component data
+  count = 0
+  @Prop(Number) readonly propA: number | undefined
+  @Prop({ default: 'default value' }) readonly propB!: string
+  @Prop([String, Boolean]) readonly propC: string | boolean | undefined
+
+  // Methods will be component methods
+  increment() {
+    this.count++
+  }
+
+  decrement() {
+    this.count--
+  }
+  test(): number{
+    return 2;
+  }
+}
+</script>
